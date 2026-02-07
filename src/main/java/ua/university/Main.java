bedc288fd1cc2f4018e5ca218a5c2b85489fa018
Roman Hvozdik <r.hvozdik@ukma.edu.ua>
2026-02-07 13:55:13 +0200

    add start menu with available commands

--- src/main/java/ua/university/Main.java
+++ src/main/java/ua/university/Main.java
@@ -1,28 +1,41 @@
 package ua.university;
+import ua.university.repository.StudentRepository;
 import ua.university.service.InputProcessor;
 import ua.university.service.InputValidator;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     /** Adds input scanner to the program */
     private static final Scanner scanner = new Scanner(System.in);
     /** Adds input validator to the program */
     private static final InputValidator inputValidator = new InputValidator();
-    /** Adds input processor to the program */
-    private static final InputProcessor inputProcessor = new InputProcessor();
+
+    private static final StudentRepository studentRepository = new StudentRepository();
+
+    private static final InputProcessor inputProcessor = new InputProcessor(studentRepository);
 
     /** Adds a list of access levels */
     private enum AccessLevel {user, manager, admin}
     /** Utilizes the list of access levels */
-    private static AccessLevel accessLevel = AccessLevel.user;
-
+    private static AccessLevel accessLevel = AccessLevel.manager;
     /** Defines if program is running (true by default) */
     protected static boolean programRunning = true;
 
     public static void main(String[] args) {
+        System.out.println();
+        System.out.println("╔════════════════════════════════════════════╗");
+        System.out.println("║   Вас вітає система DigiUni Registry!      ║");
+        System.out.println("╚════════════════════════════════════════════╝");
+        System.out.println();
+
+        System.out.println(" 1. show student - Показати список студентів");
+        System.out.println(" 2. find student [by id, full_name] - Знайти студента");
+        System.out.println(" 3. add student     - Додати студента");
+        System.out.println(" 4. update student  - Оновити інформацію існуючого студента");
+        System.out.println(" 5. delete student  - Видалити студента");
+        System.out.println();
+
         while (programRunning) {
             // Defines user access before every command
             switch (accessLevel){
@@ -37,7 +50,6 @@ public class Main {
             }
         }
     }
-
     /**
      * Makes program operate a request
      * @param lineStart String that starts every line
