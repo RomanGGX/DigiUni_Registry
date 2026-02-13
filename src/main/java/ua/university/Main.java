d9c7e5daa3af35b7d0695e17a18f88dc67c2642b
Roman Hvozdik <r.hvozdik@ukma.edu.ua>
2026-02-13 11:04:02 +0200

    add Initializer for all examples

--- src/main/java/ua/university/Main.java
+++ src/main/java/ua/university/Main.java
@@ -1,12 +1,17 @@
 package ua.university;
-import ua.university.repository.StudentRepository;
+import ua.university.domain.University;
+import ua.university.repository.*;
 import ua.university.service.InputProcessor;
 
 public class Main {
 
+    private static final UniversityRepository universityRepository = new UniversityRepository();
+    private static final FacultyRepository facultyRepository = new FacultyRepository();
+    private static final DepartmentRepository departmentRepository = new DepartmentRepository();
     private static final StudentRepository studentRepository = new StudentRepository();
+    private static final TeacherRepository teacherRepository = new TeacherRepository();
 
-    private static final InputProcessor inputProcessor = new InputProcessor(studentRepository);
+    private static final InputProcessor inputProcessor = new InputProcessor(studentRepository, universityRepository, facultyRepository, departmentRepository, teacherRepository);
 
     /** Adds a list of access levels */
     private enum AccessLevel {user, manager, admin}
@@ -14,6 +19,8 @@ public class Main {
     private static AccessLevel accessLevel = AccessLevel.manager;
 
     public static void main(String[] args) {
+        Initializer.initializeAll(studentRepository, facultyRepository, departmentRepository, teacherRepository);
+
         System.out.println();
         System.out.println("╔════════════════════════════════════════════╗");
         System.out.println("║   Вас вітає система DigiUni Registry!      ║");
