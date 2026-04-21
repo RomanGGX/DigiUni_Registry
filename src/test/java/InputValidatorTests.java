9eec4898a4464584f3d320fb2ecf0abbee107245
SecondMelon <y.voloshanovych@ukma.edu.ua>
2026-04-21 12:32:38 +0300

    feat: extend tests for validations

--- src/test/java/InputValidatorTests.java
+++ src/test/java/InputValidatorTests.java
@@ -61,4 +61,106 @@ public class InputValidatorTests {
     void checkStudentIDShouldReturnFormatedID (String argument, String expected) {
         assertEquals(expected, validator.checkStudentID(argument));
     }
+
+    @ParameterizedTest
+    @ValueSource (ints = {0, -2, 7, 10})
+    void checkCourseShouldReturnNegative(int course) { assertEquals(-1, validator.checkCourse(course)); }
+
+    @ParameterizedTest
+    @ValueSource (ints = {1, 2, 3, 4, 5, 6})
+    void checkCourseShouldReturnCourse(int course) { assertEquals(course, validator.checkCourse(course)); }
+
+    @ParameterizedTest
+    @ValueSource (ints = {1991, 1990, 15, 26, 1500})
+    void checkYearEnrollShouldReturnNegative(int year) { assertEquals(-1, validator.checkYearEnroll(year)); }
+
+    @ParameterizedTest
+    @ValueSource (ints = {1992, 2000, 2023, 2015})
+    void checkYearEnrollShouldReturnYear(int year) { assertEquals(year, validator.checkYearEnroll(year)); }
+
+    @ParameterizedTest
+    @ValueSource (strings = {"32.09.2015", "30.02.2015", "15.13.2015", "5.03.2015", "09.3.2015", "20.11.15"})
+    void checkFullDateShouldReturnNegative(String date) { assertEquals("-1", validator.checkFullDate(date)); }
+
+    @ParameterizedTest
+    @CsvSource ({
+            "15.10.2012, 15.10.2012",
+            "29.02.2020, 29.02.2020",
+            "31.12.2015, 31.12.2015"
+    })
+    void checkFullDateShouldReturnFormattedFullDate(String argument, String expected) {
+        assertEquals(expected, validator.checkFullDate(argument));
+    }
+
+    @Test
+    void checkStudyFormShouldReturnNegative() { assertEquals("-1", validator.checkStudyForm("Щось інше")); }
+
+    @ParameterizedTest
+    @CsvSource ({
+            "Бюджет, бюджет",
+            "КОНТРАКТ, контракт",
+            "бюджет, бюджет"
+    })
+    void checkStudyFormShouldReturnFormattedStudyForm(String argument, String expected) {
+        assertEquals(expected, validator.checkStudyForm(argument));
+    }
+
+    @Test
+    void checkAcademicDegreeShouldReturnNegative() { assertEquals("-1", validator.checkAcademicDegree("Щось інше")); }
+
+    @ParameterizedTest
+    @CsvSource ({
+            "ДОКТОР ФІЛОСОФІЇ, доктор філософії",
+            "ДоКтОр нАуК, доктор наук"
+    })
+    void checkAcademicDegreeShouldReturnFormattedAcademicDegree(String argument, String expected) {
+        assertEquals(expected, validator.checkAcademicDegree(argument));
+    }
+
+    @Test
+    void checkAcademicTitleShouldReturnNegative() { assertEquals("-1", validator.checkAcademicTitle("Щось інше")); }
+
+    @ParameterizedTest
+    @CsvSource ({
+            "ДОЦЕНТ, доцент",
+            "старшиЙ дослідниК, старший дослідник",
+            "ПрОфЕсОр, професор"
+    })
+    void checkAcademicTitleShouldReturnFormattedAcademicTitle(String argument, String expected) {
+        assertEquals(expected, validator.checkAcademicTitle(argument));
+    }
+
+    @Test
+    void checkStatusShouldReturnNegative() { assertEquals("-1", validator.checkStatus("Щось інше")); }
+
+    @ParameterizedTest
+    @CsvSource ({
+            "НАВЧАЄТЬСЯ, навчається",
+            "академвідпусткА, академвідпустка",
+            "ВіДрАхОвАнИй, відрахований"
+    })
+    void checkStatusShouldReturnFormattedStatus(String argument, String expected) {
+        assertEquals(expected, validator.checkStatus(argument));
+    }
+
+    @ParameterizedTest
+    @ValueSource (strings = {".invalid_email@gmail.com", "cool_email@gmail.", "even_cooler..email@gmail.com"})
+    void checkEmailShouldReturnNegative(String argument) { assertEquals("-1", validator.checkEmail(argument)); }
+
+    @ParameterizedTest
+    @CsvSource ({
+            "The.Real_Slim.Shady228@gmail.gmail.gmail.com, the.real_slim.shady228@gmail.gmail.gmail.com",
+            "cOOl1UsEr2nAmE3And4EmAIl5dOwnlOAd6wAlpApErs@ukr.net.any.other.domain.will.not.work.haha, cool1user2name3and4email5download6walpapers@ukr.net.any.other.domain.will.not.work.haha"
+    })
+    void checkEmailShouldReturnFormattedEmail(String argument, String expected) {
+        assertEquals(expected, validator.checkEmail(argument));
+    }
+
+    @ParameterizedTest
+    @ValueSource (strings = {"+3805", "2942429185", "+1929401938592929485820", "Suddenly a dog"})
+    void checkPhoneNumberShouldReturnNegative(String argument) { assertEquals("-1", validator.checkPhoneNumber(argument)); }
+
+    @ParameterizedTest
+    @ValueSource (strings = {"+3802819572", "+502918", "+39102859018472"})
+    void checkPhoneNumberShouldReturnPhoneNumber(String argument) { assertEquals(argument, validator.checkPhoneNumber(argument)); }
 }
